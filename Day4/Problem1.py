d92c3eb41afaada4876b1e8730fac02b57a2002e
arjungravi1@gmail.com <arjungravi1@gmail.com>
2024-12-05 09:15:43 +0530

    facing bugs

--- Day4/Problem1.py
+++ Day4/Problem1.py
@@ -1,13 +1,18 @@
-with open('Day4/input.txt') as f:
+with open('Day4/input2.txt') as f:
     data = f.read()
-LEN = 140
 XMAS_count = 0
 d = []  # d is the data as a matrix
+for i in data.split('\n'):
+    if i:
+        d.append(list(i))
+LEN = len(d)    
+print('LEN:', LEN)
 
 def look_forward(row, col):
     if col+3<LEN:
         if d[row][col] +d[row][col+1] +d[row][col+2] +d[row][col+3] == 'XMAS':
             # print(d[row][col] +d[row][col+1] +d[row][col+2] +d[row][col+3])
+            print(row,col,'look_forward')
             return 1
     return 0
 
@@ -15,6 +20,7 @@ def look_backward(row, col):
     if col-3>0:
         if d[row][col] +d[row][col-1] +d[row][col-2] +d[row][col-3] == 'XMAS':
             # print(d[row][col] +d[row][col+1] +d[row][col+2] +d[row][col+3])
+            print(row,col,'look_backward')
             return 1
     return 0
 
@@ -22,6 +28,7 @@ def look_down(row, col):
     if row+3<LEN:
         if d[row][col] +d[row+1][col] +d[row+2][col] +d[row+3][col] == 'XMAS':
             # print(d[row][col] +d[row][col+1] +d[row][col+2] +d[row][col+3])
+            print(row,col,'look_down')
             return 1
     return 0
 
@@ -29,22 +36,41 @@ def look_up(row, col):
     if row-3>0:
         if d[row][col] +d[row-1][col] +d[row-2][col] +d[row-3][col] == 'XMAS':
             # print(d[row][col] +d[row][col+1] +d[row][col+2] +d[row][col+3])
+            print(row,col,'look_up')
+            return 1
+    return 0
+
+def look_right_down_diagonal(row, col):
+    if row+3<LEN and col+3<LEN:
+        if d[row][col] +d[row+1][col+1] +d[row+2][col+2] +d[row+3][col+3] == 'XMAS':
+            print(row,col,'look_right_down_diagonal')
+            return 1
+    return 0
+
+def look_right_up_diagonal(row, col):
+    if row-3>0 and col+3<LEN:
+        if d[row][col] +d[row-1][col+1] +d[row-2][col+2] +d[row-3][col+3] == 'XMAS':
+            print(row,col,'look_right_up_diagonal')
+            return 1
+    return 0
+
+def look_left_down_diagonal(row, col):
+    if row+3<LEN and col-3>0:
+        if d[row][col] +d[row+1][col-1] +d[row+2][col-2] +d[row+3][col-3] == 'XMAS':
+            print(row,col,'look_left_down_diagonal')
+            return 1
+    return 0
+
+def look_left_up_diagonal(row, col):
+    if row-3>0 and col-3>0:
+        if d[row][col] +d[row-1][col-1] +d[row-2][col-2] +d[row-3][col-3] == 'XMAS':
+            print(row,col,'look_left_up_diagonal')
             return 1
     return 0
 
 
 
 
-
-
-
-
-
-
-for i in data.split('\n'):
-    if i:
-        d.append(list(i))
-# print(len(d), len(d[0])) # 140 140
 
 for row in range(len(d)):
     for col in range(len(d[row])):
@@ -55,6 +81,10 @@ for row in range(len(d)):
             XMAS_count += look_backward(row, col)
             XMAS_count += look_up(row, col)
             XMAS_count += look_down(row, col)
+            XMAS_count += look_right_down_diagonal(row, col)
+            XMAS_count += look_right_up_diagonal(row, col)
+            XMAS_count += look_left_down_diagonal(row, col)
+            XMAS_count += look_left_up_diagonal(row, col)
 
 print(XMAS_count)
 
