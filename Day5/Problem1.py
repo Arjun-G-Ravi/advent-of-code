82d7da0d0eced6fb785c9d870514ca07a656ea06
arjungravi1@gmail.com <arjungravi1@gmail.com>
2024-12-06 07:41:26 +0530

    confused

--- Day5/Problem1.py
+++ Day5/Problem1.py
@@ -1,17 +1,20 @@
 from print_color import print
 
-with open('input.txt', 'r') as f:
+with open('Day5/input2.txt', 'r') as f:
     data = f.read()
 
 data1, data2 = data.split('\n\n')
 data1 = data1.split('\n')
-# print(data1)
+print(data1)
 hmap = {}
 for r in data1:
     k, v = r.split('|')
-    hmap[int(v)] = int(k) # reversed order
+    if int(v) not in hmap:
+        hmap[int(v)] = [int(k)] # reversed order
+    else:
+        hmap[int(v)].append(int(k))
 
-print(hmap)
+print(hmap, color='b')
 
 data2 = data2.split('\n')
 # print(data2)
@@ -23,18 +26,31 @@ for r in data2:
         row = list(map(int, row))
         # print(row)
         for i in range(len(row)):
-            print(row, color='g')
-            if row[i] in hmap:
+            print('Element:', row[i], color='g')
+            if row[i] in hmap and max([hmap[row[i]][j] in row for j in range(len(hmap[row[i]]))]):
                 print(row[i])
-                if not (hmap[row[i]] in row[:i]):
-                    print(row[i])
-                    print(row[:i])
-                    print('here')
-                    is_correct = False
-                    break
-    break
+                # print(list(hmap[row[i]][j] in row[:i] for j in range(len(hmap[row[i]]))))
+                # if not max(hmap[row[i]][j] in row[:i] for j in range(len(hmap[row[i]]))): pass
+                for j in range(len(hmap[row[i]])):
+                    print(j)
+                    if hmap[row[i]][j] in row[:i]:
+                        print('cow', color='r')
+                        print(row, color='r')
+                
+                        # print(row[i])
+                        # print(row[:i])
+                        # print('here')
+                        is_correct = False
+                        break
+            if is_correct == False:
+                break
+
+    
     if is_correct:
+        print(row, 'is correct', color='purple')
         ct += 1
         print(row, color='r')
 
 print(ct)
+
+{53: [47, 75, 61, 97], 13: [97, 61, 29, 47, 75, 53], 61: [97, 47, 75], 47: [97, 75], 29: [75, 97, 53, 61, 47], 75: [97]}
